d466ea09ffdf82887ab99eb2c47f145ec9a32622
Andrii Khimich <khimicha@etu.univ-grenoble-alpes.fr>
2020-01-10 13:12:14 +0100

    Refactoring: removing obsolete syntax structures.

--- src/main/java/backend/Ast.java
+++ src/main/java/backend/Ast.java
@@ -2,32 +2,6 @@ package backend;
 
 import java.util.*;
 
-class Unit extends Exp {
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Bool extends Exp {
-    final boolean b;
-
-    Bool(boolean b) {
-        this.b = b;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
 class Int extends Exp {
     final int i;
 
@@ -43,13 +17,11 @@ class Int extends Exp {
     }
 }
 
-class Float extends Exp {
-    float f;
-    Label l;
+class Var extends Exp {
+    final Id id;
 
-    Float(float f, Label l) {
-        this.f = f;
-        this.l = l;
+    Var(Id id) {
+        this.id = id;
     }
 
     <E> E accept(ObjVisitor<E> v) {
@@ -60,16 +32,30 @@ class Float extends Exp {
     }
 }
 
-class Not extends Exp {
-    final Exp e;
+class Nop extends Exp {
+    Nop() {
 
-    Not(Exp e) {
-        this.e = e;
+    }
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class Fun extends Exp {
+    final Label l;
+
+    Fun(Label l) {
+        this.l = l;
     }
 
     <E> E accept(ObjVisitor<E> v) {
         return v.visit(this);
     }
+
     void accept(Visitor v) {
         v.visit(this);
     }
@@ -80,40 +66,6 @@ class Neg extends Exp {
 
     Neg(Id id) {
         this.id = id;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Add extends Exp {
-    final Id id;
-    final Exp e;
-
-    Add(Id id, Exp e) {
-        this.id = id;
-        this.e = e;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Sub extends Exp {
-    final Id id;
-    final Exp e;
-
-    Sub(Id id, Exp e) {
-        this.id = id;
-        this.e = e;
     }
 
     <E> E accept(ObjVisitor<E> v) {
@@ -198,6 +150,113 @@ class FDiv extends Exp {
     FDiv(Id id1, Id id2) {
         this.id1 = id1;
         this.id2 = id2;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class New extends Exp {
+    final Exp size;
+
+    New(Exp size) {
+        this.size = size;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class Add extends Exp {
+    final Id id;
+    final Exp e;
+
+    Add(Id id, Exp e) {
+        this.id = id;
+        this.e = e;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class Sub extends Exp {
+    final Id id;
+    final Exp e;
+
+    Sub(Id id, Exp e) {
+        this.id = id;
+        this.e = e;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class Get extends Exp {
+    final Id base;
+    final Exp offset;
+
+    Get(Id base, Exp offset) {
+        this.base = base;
+        this.offset = offset;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class Put extends Exp {
+    final Id base;
+    final Exp offset;
+    final Id dest;
+
+    Put(Id base, Exp offset, Id dest) {
+        this.base = base;
+        this.offset = offset;
+        this.dest = dest;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class If extends Exp {
+    final Exp cond;
+    final Exp e1;
+    final Exp e2;
+
+    If(Exp cond, Exp e1, Exp e2) {
+        this.cond = cond;
+        this.e1 = e1;
+        this.e2 = e2;
     }
 
     <E> E accept(ObjVisitor<E> v) {
@@ -293,78 +352,6 @@ class FLE extends Exp {
     }
 }
 
-class If extends Exp {
-    final Exp cond;
-    final Exp e1;
-    final Exp e2;
-
-    If(Exp cond, Exp e1, Exp e2) {
-        this.cond = cond;
-        this.e1 = e1;
-        this.e2 = e2;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Let extends Exp {
-    final Id id;
-    final Type t;
-    final Exp e1;
-    final Exp e2;
-
-    Let(Id id, Type t, Exp e1, Exp e2) {
-        this.id = id;
-        this.t = t;
-        this.e1 = e1;
-        this.e2 = e2;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Var extends Exp {
-    final Id id;
-
-    Var(Id id) {
-        this.id = id;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class LetRec extends Exp {
-    final FunDef fd;
-    final Exp e;
-
-    LetRec(FunDef fd, Exp e) {
-        this.fd = fd;
-        this.e = e;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
 class Call extends Exp {
     final Label f;
     final List<Id> args;
@@ -399,31 +386,15 @@ class AppClosure extends Exp {
     }
 }
 
-class Tuple extends Exp {
-    final List<Exp> es;
-
-    Tuple(List<Exp> es) {
-        this.es = es;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class LetTuple extends Exp {
-    final List<Id> ids;
-    final List<Type> ts;
+class Let extends Exp {
+    final Id id;
+    final Type t;
     final Exp e1;
     final Exp e2;
 
-    LetTuple(List<Id> ids, List<Type> ts, Exp e1, Exp e2) {
-        this.ids = ids;
-        this.ts = ts;
+    Let(Id id, Type t, Exp e1, Exp e2) {
+        this.id = id;
+        this.t = t;
         this.e1 = e1;
         this.e2 = e2;
     }
@@ -431,89 +402,6 @@ class LetTuple extends Exp {
     <E> E accept(ObjVisitor<E> v) {
         return v.visit(this);
     }
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class New extends Exp {
-    final Exp size;
-
-    New(Exp size) {
-        this.size = size;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Get extends Exp {
-    final Id base;
-    final Exp offset;
-
-    Get(Id base, Exp offset) {
-        this.base = base;
-        this.offset = offset;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Put extends Exp {
-    final Id base;
-    final Exp offset;
-    final Id dest;
-
-    Put(Id base, Exp offset, Id dest) {
-        this.base = base;
-        this.offset = offset;
-        this.dest = dest;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Nop extends Exp {
-    Nop() {
-
-    }
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
-    void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-
-class Fun extends Exp {
-    final Label l;
-
-    Fun(Label l) {
-        this.l = l;
-    }
-
-    <E> E accept(ObjVisitor<E> v) {
-        return v.visit(this);
-    }
-
     void accept(Visitor v) {
         v.visit(this);
     }
@@ -532,4 +420,38 @@ class FunDef {
         this.e = e;
     }
 
+}
+
+class LetRec extends Exp {
+    final FunDef fd;
+    final Exp e;
+
+    LetRec(FunDef fd, Exp e) {
+        this.fd = fd;
+        this.e = e;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+    void accept(Visitor v) {
+        v.visit(this);
+    }
+}
+
+class Float extends Exp {
+    float f;
+    Label l;
+
+    Float(float f, Label l) {
+        this.f = f;
+        this.l = l;
+    }
+
+    <E> E accept(ObjVisitor<E> v) {
+        return v.visit(this);
+    }
+    void accept(Visitor v) {
+        v.visit(this);
+    }
 }
