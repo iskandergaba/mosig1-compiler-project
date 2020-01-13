943f3ed00889dcce01b7d6111620c1582b8e56a6
Louis Boulanger <louis.boulanger@etu.univ-grenoble-alpes.fr>
2020-01-13 17:43:55 +0100

    First version of ARM code generation

--- src/main/java/backend/Main.java
+++ src/main/java/backend/Main.java
@@ -2,8 +2,11 @@ package backend;
 
 import java_cup.runtime.*;
 import java.io.*;
+<<<<<<< HEAD
 import java.util.*;
 import frontend.*;
+=======
+>>>>>>> First version of ARM code generation
 
 public class Main {
   static public void main(String argv[]) {
@@ -11,8 +14,19 @@ public class Main {
       Parser p = new Parser(new Lexer(new FileReader(argv[0])));
       Exp expression = (Exp) p.parse().value;
       assert (expression != null);
+<<<<<<< HEAD
       // expression.accept(new PrintVisitor());
       expression.accept(new RegAllocVisitor());
+=======
+
+      System.out.println("# Register allocation");
+      RegAllocVisitor v = new RegAllocVisitor();
+      expression.accept(v);
+
+      System.out.println("# ARM generation");
+      System.out.println(expression.accept(new ARMVisitor(v.memory)));
+
+>>>>>>> First version of ARM code generation
     } catch (Exception e) {
       e.printStackTrace();
     }
