e413ef7a85f451776ed3ea4f01611f0f57081842
Andrii Khimich <khimicha@etu.univ-grenoble-alpes.fr>
2020-01-13 16:33:03 +0100

    Basic register allocation strategy implementation. Added variable spilling.

--- src/main/java/backend/Main.java
+++ src/main/java/backend/Main.java
@@ -6,15 +6,15 @@ import java.util.*;
 import frontend.*;
 
 public class Main {
-  static public void main(String argv[]) {    
+  static public void main(String argv[]) {
     try {
       Parser p = new Parser(new Lexer(new FileReader(argv[0])));
       Exp expression = (Exp) p.parse().value;
       assert (expression != null);
-      expression.accept(new PrintVisitor());
+      // expression.accept(new PrintVisitor());
+      expression.accept(new RegAllocVisitor());
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 }
-
