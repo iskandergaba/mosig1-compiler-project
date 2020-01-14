fac5de8a1d5b60ade62654e147d6f4922d065aa1
Iskander Gaba <iskandergaba@gmail.com>
2020-01-14 17:13:17 +0100

    Implemented Let reducing

---
+++ src/main/java/frontend/LetReducer.java
@@ -0,0 +1,122 @@
+package frontend;
+
+import java.util.Stack;
+
+class LetReducer implements ObjVisitor<Exp> {
+
+    Stack<Let> letStack = new Stack<>();
+
+    public Exp visit(Unit e) {
+        return e;
+    }
+
+    public Exp visit(Bool e) {
+        return e;
+    }
+
+    public Exp visit(Int e) {
+        return e;
+    }
+
+    public Exp visit(Float e) {
+        return e;
+    }
+
+    public Exp visit(Not e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Neg e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Add e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Sub e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(FNeg e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(FAdd e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(FSub e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(FMul e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(FDiv e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Eq e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(LE e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(If e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Let e) throws Exception {
+        return insert(e, e.e1.accept(this));
+    }
+
+    public Exp visit(Var e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(LetRec e) throws Exception {
+        return new LetRec(e.fd, e.e.accept(this));
+    }
+
+    public Exp visit(App e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Tuple e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(LetTuple e) throws Exception {
+        return new LetTuple(e.ids, e.ts, e.e1.accept(this), e.e2.accept(this));
+    }
+
+    public Exp visit(Array e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Get e) throws Exception {
+        return e;
+    }
+
+    public Exp visit(Put e) throws Exception {
+        return e;
+    }
+
+    private Exp insert(Let outerLet, Exp e) throws Exception {
+        if (e instanceof Let) {
+            Let let = (Let)e;
+            return new Let(let.id, let.t, let.e1, insert(outerLet, let.e2));
+        } else if (e instanceof LetRec) {
+            LetRec letRec = (LetRec)e;
+            return new LetRec(letRec.fd, insert(outerLet, letRec.e));
+        } else if (e instanceof LetTuple) {
+            LetTuple letTuple = (LetTuple)e;
+            return new LetTuple(letTuple.ids, letTuple.ts, letTuple.e1, insert(outerLet, letTuple.e2));
+        }
+        return new Let(outerLet.id, outerLet.t, e, outerLet.e2.accept(this));
+    }
+}
