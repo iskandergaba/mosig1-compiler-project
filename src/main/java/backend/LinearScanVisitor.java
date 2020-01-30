426b1f7bda84dd79b1cc4919152136c987590191
Andrii Khimich <khimicha@etu.univ-grenoble-alpes.fr>
2020-01-30 17:32:23 +0100

    Description update

--- src/main/java/backend/LinearScanVisitor.java
+++ src/main/java/backend/LinearScanVisitor.java
@@ -9,22 +9,44 @@ import common.asml.*;
 import common.asml.Float;
 import common.visitor.*;
 
+/**
+ * Visits the ASML AST creates an allocation registers map using a linear scanning algorithm.
+ * If activeMax is set to 0, this is the same as a spill-all approach.
+ */
 class LinearScanVisitor implements Visitor {
+
+    // Current operatin position
     Integer position = 0;
+
+    // Name of a current scope
     String scope;
+
+    // Map of live intervals for each scope
     Map<String, List<LiveInterval>> basicBlocksMap = new HashMap<String, List<LiveInterval>>();
+
+    // Map of all live intervals
     Map<String, LiveInterval> intervalsMap;
+
+    // List of live and active intervals
     List<LiveInterval> liveIntervals, active;
+
+    // Debug mode flag
     boolean debug;
 
     public LinearScanVisitor(boolean debug) {
         this.debug = debug;
     }
 
+    // Maximum number of simultaneously allocated registers, total number of registers,
+    // FP register index, start and end of local variable registers
     final Integer activeMax = 2, registersNum = 16, fp = 11, regStart = 4, regEnd = 12;
     Boolean[] regIsFree = new Boolean[registersNum];
+
+    // Maps of registers and stack location
     Map<String, Integer> registers = new HashMap<String, Integer>();
     Map<String, Integer> locations = new HashMap<String, Integer>();
+
+    // Final memory reresentation
     Map<String, String> memory = new HashMap<String, String>();
 
     void printlnDebug(String msg) {
@@ -39,10 +61,15 @@ class LinearScanVisitor implements Visitor {
         }
     }
 
+    // Increment current position
     void updatePosition() {
         this.position++;
     }
 
+    /**
+     * Initialize a new block, updating the scope
+     * @param fname The name of function
+     */
     void startBlock(String fname) {
         this.intervalsMap = new HashMap<String, LiveInterval>();
         this.liveIntervals = new ArrayList<LiveInterval>();
@@ -50,23 +77,38 @@ class LinearScanVisitor implements Visitor {
         this.scope = fname;
     }
 
+    /**
+     * Closes the current block
+     */
     void endBlock() {
         this.intervalsMap = null;
         this.liveIntervals = null;
         this.scope = null;
     }
 
+    /**
+     * Start a new live interval for the selected variable
+     * @param vname The name of variable
+     */
     void startInterval(String vname) {
         LiveInterval liveInterval = new LiveInterval(fullPath(vname), position);
         this.liveIntervals.add(liveInterval);
         this.intervalsMap.put(fullPath(vname), liveInterval);
     }
 
+    /**
+     * Updates the range of the selected variable live interval
+     * @param fname The name of function
+     */
     void updateInterval(String vname) {
         if (locations.get(fullPath(vname)) == null)
             this.intervalsMap.get(fullPath(vname)).update(this.position);
     }
 
+    /**
+     * Generate FP offsets for function parameters
+     * @param params The list of function parameters
+     */
     void addParams(List<Id> params) {
         int i = 0;
         for (Id id: params) {
@@ -75,9 +117,15 @@ class LinearScanVisitor implements Visitor {
         }
     }
 
+    /**
+     * Get the full path of the varible in the current scope
+     * @param vname The name of variable
+     * @return Full variable path
+     */
     String fullPath(String vname) {
         return this.scope + "." + vname;
     }
+
 
     void printIntervals() {
         if (debug) {
@@ -96,6 +144,10 @@ class LinearScanVisitor implements Visitor {
         }
     }
 
+    /**
+     * Get a free register
+     * @return Index of a free register
+     */
     Integer getFreeRegister() {
         for (int i = this.regStart; i <= this.regEnd; i++) {
             if (i == fp)
@@ -118,6 +170,9 @@ class LinearScanVisitor implements Visitor {
         this.regIsFree[i] = true;
     }
 
+    /**
+     * Perform a linear scanning
+     */
     void linearScanRegisterAllocation() {
         printlnDebug("# Live intervals traversal");
 
@@ -156,6 +211,9 @@ class LinearScanVisitor implements Visitor {
         printlnDebug("");
     }
 
+    /**
+     * Free expired registers
+     */
     void expireOldInterval(LiveInterval i) {
         List<LiveInterval> expired = new ArrayList<LiveInterval>();
         for (LiveInterval j : this.active) {
@@ -170,6 +228,10 @@ class LinearScanVisitor implements Visitor {
         this.active.removeAll(expired);
     }
 
+    /**
+     * Perform a register spilling.
+     * A register to be spilled is the one is longest remaining TTL
+     */
     void spillInterval(LiveInterval i, Integer offset) {
         if (this.active.size() == 0) {
             // location
@@ -198,8 +260,6 @@ class LinearScanVisitor implements Visitor {
         }
     }
 
-
-
     @Override
     public void visit(Int e) {
         printDebug(e.i);
@@ -207,7 +267,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(Var e) {
-        // currentBlock.addGen(e.id);
         updateInterval(e.id.id);
 
         printDebug(e.id);
@@ -225,7 +284,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(Neg e) {
-        // currentBlock.addGen(e.id);
         updatePosition();
         updateInterval(e.id.id);
 
@@ -236,7 +294,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(FNeg e) {
-        // currentBlock.addGen(e.id);
         updatePosition();
         updateInterval(e.id.id);
 
@@ -247,8 +304,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(FAdd e) {
-        // currentBlock.addGen(e.id1);
-        // currentBlock.addGen(e.id2);
         updatePosition();
         updateInterval(e.id1.id);
         updateInterval(e.id2.id);
@@ -262,8 +317,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(FSub e) {
-        // currentBlock.addGen(e.id1);
-        // currentBlock.addGen(e.id2);
         updatePosition();
         updateInterval(e.id1.id);
         updateInterval(e.id2.id);
@@ -277,8 +330,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(FMul e) {
-        // currentBlock.addGen(e.id1);
-        // currentBlock.addGen(e.id2);
         updatePosition();
         updateInterval(e.id1.id);
         updateInterval(e.id2.id);
@@ -292,8 +343,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(FDiv e) {
-        // currentBlock.addGen(e.id1);
-        // currentBlock.addGen(e.id2);
         updatePosition();
         updateInterval(e.id1.id);
         updateInterval(e.id2.id);
@@ -313,7 +362,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(Add e) {
-        // currentBlock.addGen(e.id);
         updatePosition();
         updateInterval(e.id.id);
 
@@ -326,7 +374,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(Sub e) {
-        // currentBlock.addGen(e.id);
         updatePosition();
         updateInterval(e.id.id);
 
@@ -339,7 +386,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(Get e) {
-        // currentBlock.addGen(e.base);
         updatePosition();
 
         printDebug("mem (");
@@ -351,8 +397,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(Put e) {
-        // currentBlock.addGen(e.base);
-        // currentBlock.addGen(e.dest);
         updatePosition();
 
         printlnDebug("mem (");
@@ -369,20 +413,16 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(If e) {
-        // printDebug(position + ": ");
         printDebug("if ");
         e.cond.accept(this);
         printDebug(" then\n");
-        // updatePosition();
         e.e1.accept(this);
         printDebug(" else\n");
-        // updatePosition();
         e.e2.accept(this);
     }
 
     @Override
     public void visit(Eq e) {
-        // currentBlock.addGen(e.id);
         updatePosition();
         updateInterval(e.id.id);
 
@@ -395,7 +435,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(LE e) {
-        // currentBlock.addGen(e.id);
         updatePosition();
         updateInterval(e.id.id);
 
@@ -408,7 +447,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(GE e) {
-        // currentBlock.addGen(e.id);
         updatePosition();
         updateInterval(e.id.id);
 
@@ -421,8 +459,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(FEq e) {
-        // currentBlock.addGen(e.id1);
-        // currentBlock.addGen(e.id2);
         updatePosition();
         updateInterval(e.id1.id);
         updateInterval(e.id2.id);
@@ -436,8 +472,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(FLE e) {
-        // currentBlock.addGen(e.id1);
-        // currentBlock.addGen(e.id2);
         updatePosition();
         updateInterval(e.id1.id);
         updateInterval(e.id2.id);
@@ -466,8 +500,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(AppClosure e) {
-        // currentBlock.addGen(e.id);
-
         printDebug("appclo ");
         printDebug(e.id);
         updateInterval(e.id.id);
@@ -490,8 +522,6 @@ class LinearScanVisitor implements Visitor {
 
     @Override
     public void visit(Let e) {
-        // currentBlock.addKill(e.id);
-
         printDebug(position + ": ");
         printDebug("let ");
         printDebug(e.id);
@@ -520,11 +550,8 @@ class LinearScanVisitor implements Visitor {
         for (Id id: e.fd.args) {
             printDebug(" ");
             printDebug(id);
-
-            // startInterval(id.id);
         }
         printDebug(" =\n");
-        // updatePosition();
         e.fd.e.accept(this);
         printDebug("");
         endBlock();
